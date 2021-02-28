9b39c599e5105ee3197ecb6a8ee4a6c196a20995
leiu <leiuyi@outlook.com>
2021-02-28 22:57:50 +0800

    Complete the control part.

--- combinationTestDownload.js
+++ combinationTestDownload.js
@@ -1,7 +1,33 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+   if (request.todo == "download") {
+      if (request.downloadFile[0]) {
+         downloadJavaPage();
+      }
+
+      if (request.downloadFile[1]) {
+         downloadDataDictionary();
+      }
+   }
+})
+
 function downloadJavaPage() {
-   console.log(document.title);
+   console.log("// TODO downloadJavaPage");
 }
 
 function downloadDataDictionary() {
-   console.log(document.lastModified);
+
+   let nodes = document.all;
+   for (let node of nodes) {
+      if (node.nodeType = 1) {
+         switch(node.nodeName) {
+            case "A":
+               console.log(node.href)
+               break;
+            default:
+               // nothing to do ..
+         }
+      }
+   }
+
+   console.log("// TODO downloadDataDictionary");
 }
--- popup.js
+++ popup.js
@@ -3,14 +3,12 @@ window.onload = function () {
       let javaPage = document.querySelector('#javaPage');
       let dataDictionary = document.querySelector('#dataDictionary');
       
-      if (javaPage.checked) {
-         downloadJavaPage();
-      }
-
-      if (dataDictionary.checked) {
-         downloadDataDictionary();
-      }
-
+      chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+         chrome.tabs.sendMessage(tabs[0].id, {
+            todo: "download", downloadFile: [javaPage.checked, dataDictionary.checked]
+         });
+         console.log(tabs[0].id);
+      })
       this.blur();
    }
 }
